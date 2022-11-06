55d693012c354ee4039d301a32f8334f081e3da0
Sherly Ferro <she.eli.fer@gmail.com>
2022-11-05 23:45:23 -0500

    repo3

--- src/api/index.js
+++ src/api/index.js
@@ -28,22 +28,68 @@ const products = [
     },
     {
         id:4,
-        name: "RELISE",
-        category:"joya",
-        description: "RELISE JUEGO COLLAR ARETE PLATA MARIPOSA",
+        name: "GUESS",
+        category:"reloj",
+        description: "JOYERO CIERVO ORGANIZADOR BLANCO",
         price: 194,
-        discont:170.20,
-        img:"https://cdn.shopify.com/s/files/1/0274/8838/7170/products/lalique_anillo_1925_10679500_400x.png?v=1598904553"
+        discont:180.20,
+        img:"https://cdn.shopify.com/s/files/1/0274/8838/7170/products/H38416711_400x.png?v=1655915301"
     },
     {
         id:5,
+        name: "SKECHERS",
+        category:"reloj",
+        description: "JOYERO CIERVO ORGANIZADOR BLANCO",
+        price: 194,
+        discont:180.20,
+        img:"https://cdn.shopify.com/s/files/1/0274/8838/7170/products/MB125565_400x.png?v=1621396233"
+    },
+    {
+        id:6,
+        name: "RELISE",
+        category:"joya",
+        description: "ANILLO CON CULEBRA GUCCI GARDEN",
+        price: 194,
+        discont:170.20,
+        img:"https://cdn.shopify.com/s/files/1/0274/8838/7170/products/Anillo-CulebraGucci-Garden-YBC577292001022-2_300x.png?v=1602089514"
+    },
+    {
+        id:7,
+        name: "RELISE",
+        category:"joya",
+        description: "ANILLO GUCCI GHOST",
+        price: 194,
+        discont:170.20,
+        img:"https://cdn.shopify.com/s/files/1/0274/8838/7170/products/Anillo-Gucci-Ghost-YBC477339001014-2_300x.png?v=1602183923"
+    },
+    {
+        id:8,
+        name: "RELISE",
+        category:"joya",
+        description: "ANILLO ZEROI",
+        price: 194,
+        discont:170.20,
+        img:"https://cdn.shopify.com/s/files/1/0274/8838/7170/products/bvlgari-Anillo-Bzero1-354916-2_300x.png?v=1602870818"
+    },
+    {
+        id:9,
+        name: "ACCESORIO DE JOYAS",
+        category:"accesorio",
+        description: "JOYERO CIERVO ORGANIZADOR BLANCO",
+        price: 194,
+        discont:180.20,
+        img:"https://cdn.shopify.com/s/files/1/0274/8838/7170/products/Anillo-Gucci-Ghost-YBC477339001014-2_300x.png?v=1602183923"
+    },
+    {
+        id:10,
         name: "ACCESORIO DE JOYAS",
         category:"accesorio",
         description: "JOYERO CIERVO ORGANIZADOR BLANCO",
         price: 194,
         discont:180.20,
         img:"https://cdn.shopify.com/s/files/1/0274/8838/7170/products/bvlgari-Anillo-Bzero1-323533-1_400x.png?v=1602869698"
-    }
+    },
+    
 ]
 
 
