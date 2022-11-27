6d5a1b451f82f9bc1372e15dadf505e342b573a0
Sherly Ferro <she.eli.fer@gmail.com>
2022-11-27 16:08:13 -0500

    proyecto final

--- src/components/CartWidget.js
+++ src/components/CartWidget.js
@@ -1,14 +1,21 @@
-import React, {useEffect, useContext} from "react";
+import React, {useEffect, useContext, useState} from "react";
 import { Link } from "react-router-dom";
 import { DataContext } from "../context/DataContext";
 // el carrito incrementa las categorias no los productos //
 
 function CartWidget() {
+  const [total, setTotal] = useState(0)
   const { data } = useContext(DataContext)
- 
+  useEffect(() => {
+    let temp = 0
+    data.products.forEach(product => {
+      temp = temp + product.quantity
+    });
+    setTotal(temp)
+  },[data])
   return (
     <Link to="/carrito">
-      <span className="material-icons">shopping_cart</span> {data.products.length}
+      <span className="material-icons">shopping_cart</span> {total}
     </Link>
   );
 }
